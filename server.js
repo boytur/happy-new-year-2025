23664d47b0d0cfa6464a4f50e5a8d2606a348218
boytur <104257779+boytur@users.noreply.github.com>
2024-12-27 19:46:24 +0700

    first commit

--- server.js
+++ server.js
@@ -1,56 +1,29 @@
 const http = require("http");
 
 const bsospaceText = `
-  \x1b[31mBBBBB\x1b[0m    \x1b[32mSSSSS\x1b[0m   \x1b[33mOOOOO\x1b[0m   \x1b[34mSSSSS\x1b[0m   \x1b[35mPPPPP\x1b[0m    \x1b[36mAAAAA\x1b[0m   \x1b[37mCCCCC\x1b[0m   \x1b[38mEEEEE\x1b[0m
-  \x1b[31mB    B\x1b[0m  \x1b[32mS\x1b[0m       \x1b[33mO     O\x1b[0m  \x1b[34mS\x1b[0m       \x1b[35mP    P\x1b[0m  \x1b[36mA     A\x1b[0m  \x1b[37mC\x1b[0m        \x1b[38mE\x1b[0m
-  \x1b[31mBBBBB\x1b[0m   \x1b[32mSSSSS\x1b[0m   \x1b[33mO     O\x1b[0m  \x1b[34mSSSSS\x1b[0m   \x1b[35mPPPPPP\x1b[0m  \x1b[36mAAAAAA\x1b[0m  \x1b[37mC\x1b[0m        \x1b[38mEEEE\x1b[0m
-  \x1b[31mB    B\x1b[0m      \x1b[32mS\x1b[0m   \x1b[33mO     O\x1b[0m      \x1b[34mS\x1b[0m   \x1b[35mP\x1b[0m       \x1b[36mA     A\x1b[0m  \x1b[37mC\x1b[0m        \x1b[38mE\x1b[0m
-  \x1b[31mBBBBB\x1b[0m    \x1b[32mSSSSS\x1b[0m   \x1b[33mOOOOO\x1b[0m   \x1b[34mSSSSS\x1b[0m   \x1b[35mP\x1b[0m       \x1b[36mA     A\x1b[0m  \x1b[37mCCCCC\x1b[0m   \x1b[38mEEEEE\x1b[0m
+  \x1b[31mBBBBB\x1b[0m    \x1b[32mSSSSS\x1b[0m   \x1b[33mOOOOO\x1b[0m   \x1b[34mSSSSS\x1b[0m   \x1b[35mPPPPP\x1b[0m    \x1b[36mAAAAA\x1b[0m   \x1b[37mCCCCC\x1b[0m   \x1b[34mEEEEE\x1b[0m
+  \x1b[31mB    B\x1b[0m  \x1b[32mS\x1b[0m       \x1b[33mO     O\x1b[0m  \x1b[34mS\x1b[0m       \x1b[35mP    P\x1b[0m  \x1b[36mA     A\x1b[0m  \x1b[37mC\x1b[0m        \x1b[34mE\x1b[0m
+  \x1b[31mBBBBB\x1b[0m   \x1b[32mSSSSS\x1b[0m   \x1b[33mO     O\x1b[0m  \x1b[34mSSSSS\x1b[0m   \x1b[35mPPPPPP\x1b[0m  \x1b[36mAAAAAA\x1b[0m  \x1b[37mC\x1b[0m        \x1b[34mEEEE\x1b[0m
+  \x1b[31mB    B\x1b[0m      \x1b[32mS\x1b[0m   \x1b[33mO     O\x1b[0m      \x1b[34mS\x1b[0m   \x1b[35mP\x1b[0m       \x1b[36mA     A\x1b[0m  \x1b[37mC\x1b[0m        \x1b[34mE\x1b[0m
+  \x1b[31mBBBBB\x1b[0m    \x1b[32mSSSSS\x1b[0m   \x1b[33mOOOOO\x1b[0m   \x1b[34mSSSSS\x1b[0m   \x1b[35mP\x1b[0m       \x1b[36mA     A\x1b[0m  \x1b[37mCCCCC\x1b[0m   \x1b[34mEEEEE\x1b[0m
 `;
 
-
-// ASCII Art: พลุหลากหลายตำแหน่ง และต้นคริสต์มาสที่มีไฟ
-const fireworksFrames = [
-  `
-                 \x1b[33m🌟\x1b[0m                      \x1b[34m🌟\x1b[0m                     \x1b[35m🌟\x1b[0m
-                \x1b[32m🎆🎆🎆\x1b[0m                  \x1b[36m🎆🎆🎆\x1b[0m                 \x1b[31m🎆🎆🎆\x1b[0m
-             \x1b[33m🎇🎇🎇🎇🎇\x1b[0m              \x1b[34m🎇🎇🎇🎇🎇\x1b[0m             \x1b[35m🎇🎇🎇🎇🎇\x1b[0m
-          \x1b[32m🎆🎆🎆🎆🎆🎆🎆\x1b[0m        \x1b[36m🎆🎆🎆🎆🎆🎆🎆\x1b[0m        \x1b[31m🎆🎆🎆🎆🎆🎆🎆\x1b[0m
-             \x1b[33m🎇🎇🎇🎇🎇\x1b[0m          \x1b[34m🎇🎇🎇🎇🎇\x1b[0m           \x1b[35m🎇🎇🎇🎇🎇\x1b[0m
-                \x1b[32m🎆🎆🎆\x1b[0m                \x1b[36m🎆🎆🎆\x1b[0m             \x1b[31m🎆🎆🎆\x1b[0m
-                \x1b[33m✨  ✨ ✨\x1b[0m              \x1b[34m✨  ✨ ✨\x1b[0m           \x1b[35m✨  ✨ ✨\x1b[0m
-  `,
-  `
-          \x1b[33m🎆🎆🎇🎆🎆🎇\x1b[0m              \x1b[34m🎆🎆🎆🎆🎆🎆🎆\x1b[0m          \x1b[31m🎆🎆🎆🎆🎆🎆🎆\x1b[0m
-       \x1b[32m🌟\x1b[0m  \x1b[33m🎇🎇🎇🎇🎇🎇🎇🎇🎇\x1b[0m        \x1b[36m🎇🎇🎆🎆🎆🎆🎆🎆🎆🎆\x1b[0m  \x1b[31m🎇🎇🎇🎇🎇🎇🎇🎇\x1b[0m
-      \x1b[34m🎇🎇🎆🎆🎆🎆🎆🎆🎆🎆🎆🎇\x1b[0m    \x1b[33m🎆🎇🎆🎆🎆🎆🎆🎇🎆🎆🎇\x1b[0m  \x1b[32m🎆🎆🎆🎇🎆🎆🎆🎆\x1b[0m
-        \x1b[35m🎆🎇🎇🎆🎇🎇🎆🎇🎆🎆\x1b[0m      \x1b[33m🎇🎇🎇🎇🎆🎆🎆🎇🎆🎇🎆\x1b[0m    \x1b[36m🎇🎆🎆🎇🎇\x1b[0m
-          \x1b[31m✨   ✨      ✨       ✨\x1b[0m      \x1b[32m✨    ✨      ✨\x1b[0m         \x1b[34m✨\x1b[0m
-  `,
-  `
-               \x1b[33m🌟\x1b[0m      \x1b[34m🌟\x1b[0m       \x1b[31m🌟\x1b[0m  \x1b[32m🌟\x1b[0m    \x1b[36m🌟\x1b[0m      \x1b[35m🌟\x1b[0m         \x1b[33m🌟\x1b[0m
-              \x1b[31m🎇🎇\x1b[0m    \x1b[34m🎆🎆\x1b[0m   \x1b[33m🎇🎆🎇\x1b[0m \x1b[32m🎆🎆\x1b[0m  \x1b[35m🎇🎆🎆🎆🎇🎆\x1b[0m       \x1b[36m🎇🎇🎇🎇\x1b[0m
-         \x1b[32m🎆🎆🎆🎆🎆\x1b[0m  \x1b[36m🎇🎆🎇🎆🎇🎆🎆🎇🎆🎆🎇🎇🎆🎆🎆🎆🎆🎇🎆🎆🎆\x1b[0m
-        \x1b[31m🎇🎆🎆🎆🎆🎆🎇🎆🎇🎆🎆🎇🎆🎆🎇🎇🎆🎇🎆🎆🎇🎇🎆🎆🎇🎆\x1b[0m
-                \x1b[34m✨   ✨     ✨\x1b[0m          \x1b[33m✨        ✨\x1b[0m       \x1b[31m✨\x1b[0m      \x1b[32m✨\x1b[0m
-  `,
-];
 
 // Christmas tree with colorful lights
 const treeWithLights = `
                                  \x1b[32m🌟\x1b[0m                  
-                                 \x1b[33m* *\x1b[0m                   
-                                \x1b[35m* * *\x1b[0m                 
-                               \x1b[34m* * * *\x1b[0m                
-                              \x1b[36m* * * * *\x1b[0m               
-                             \x1b[31m* * * * * *\x1b[0m              
-                            \x1b[32m* * * * * * *\x1b[0m             
-                           \x1b[33m* * * * * * * *\x1b[0m            
-                          \x1b[34m* * * * * * * * *\x1b[0m           
-                         \x1b[35m* * * * * * * * * *\x1b[0m          
-                        \x1b[36m* * * * * * * * * * *\x1b[0m         
-                                 \x1b[32m||| \x1b[0m                    
-                                 \x1b[33m||| \x1b[0m                    
+                                 \x1b[33m* *\x1b[0m                    
+                                \x1b[35m* * *\x1b[0m                
+                               \x1b[34m* * * *\x1b[0m              
+                              \x1b[36m* * * * *\x1b[0m            
+                             \x1b[31m* * * * * *\x1b[0m           
+                            \x1b[32m* * * * * * *\x1b[0m          
+                           \x1b[33m* * * * * * * *\x1b[0m        
+                          \x1b[34m* * * * * * * * *\x1b[0m     
+                         \x1b[35m* * * * * * * * * *\x1b[0m      
+                        \x1b[36m* * * * * * * * * * *\x1b[0m    
+                                 \x1b[32m||| \x1b[0m                 
+                                 \x1b[33m||| \x1b[0m                 
              🎄 \x1b[31mMerry Christmas & Happy New Year 2025!\x1b[0m 🎄
 `;
 
@@ -58,21 +31,11 @@ const treeWithLights = `
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
 
-  let frame = 0;
+  // Display the tree and message once
+  res.write(treeWithLights);
+  res.write(bsospaceText);
 
-  // Animation loop
-  const interval = setInterval(() => {
-    res.write("\x1b[2J\x1b[H"); // Clear screen
-    res.write(fireworksFrames[frame % fireworksFrames.length]); // Fireworks animation
-    res.write(treeWithLights); // Christmas tree with lights
-    res.write(bsospaceText); // BSO Space message
-    frame++;
-  }, 300); // Change animation every 300ms
-
-  // Stop animation after 15 seconds
-  setTimeout(() => {
-    clearInterval(interval);
-  }, 15000);
+  res.end();
 });
 
 // Start server on port 2025
